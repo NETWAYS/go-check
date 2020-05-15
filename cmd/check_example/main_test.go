d44894248ecfe8dd13acd4204a8aada115360b8e
Markus Frosch <markus.frosch@netways.de>
2020-05-15 12:06:11 +0200

    Add testhelper

---
+++ cmd/check_example/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/NETWAYS/go-check/testhelper"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"regexp"
+	"testing"
+)
+
+func TestMyMain(t *testing.T) {
+	stdout := testhelper.RunMainTest(main, "--help")
+	assert.Regexp(t, regexp.MustCompile(`would exit with code 3`), stdout)
+
+	stdout = testhelper.RunMainTest(main, "--warning", "20")
+	assert.Regexp(t, regexp.MustCompile(`^OK - value is 10\nwould exit with code 0\n$`), stdout)
+
+	stdout = testhelper.RunMainTest(main, "--warning", "10", "--value", "11")
+	assert.Regexp(t, regexp.MustCompile(`^WARNING - value is 11\nwould exit with code 1\n$`), stdout)
+}
+
+func TestMain(m *testing.M) {
+	testhelper.EnableTestMode()
+	os.Exit(m.Run())
+}
---
+++ testhelper/for_main.go
@@ -0,0 +1,58 @@
+package testhelper
+
+import (
+	"bytes"
+	"github.com/NETWAYS/go-check"
+	"io"
+	"os"
+)
+
+// Execute main function from a main package, while capturing its stdout
+//
+// You will need to pass `main`, since the function won't have access to the package's namespace
+func RunMainTest(f func(), args ...string) string {
+	base := []string{"check_with_go_test"}
+	origArgs := os.Args
+	os.Args = append(base, args...)
+	stdout := CaptureStdout(f)
+	os.Args = origArgs
+	return stdout
+}
+
+// Enable test mode by disabling the default exit behavior, so go test won't fail with plugin states
+func EnableTestMode() {
+	// disable actual exit
+	check.AllowExit = false
+
+	// disable stack trace for the example
+	check.PrintStack = false
+}
+
+// Disable test mode behavior again
+//
+// Optional after testing has been done
+func DisableTestMode() {
+	// disable actual exit
+	check.AllowExit = true
+
+	// disable stack trace for the example
+	check.PrintStack = true
+}
+
+// Capture the output of the go program while running function f
+//
+// Source https://gist.github.com/mindscratch/0faa78bd3c0005d080bf
+func CaptureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	_, _ = io.Copy(&buf, r)
+	return buf.String()
+}
