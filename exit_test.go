bce255e32607c52856eb45d2757988f7a0172005
Lorenz Kästle <lorenz.kaestle@netways.de>
2022-09-08 11:54:08 +0200

    Fix test cases

--- exit_test.go
+++ exit_test.go
@@ -8,26 +8,26 @@ import (
 
 func ExampleExit() {
 	Exitf(OK, "Everything is fine - value=%d", 42)
-	// Output: OK - Everything is fine - value=42
+	// Output: [OK] - Everything is fine - value=42
 	// would exit with code 0
 }
 
 func ExampleExitf() {
 	Exitf(OK, "Everything is fine - value=%d", 42)
-	// Output: OK - Everything is fine - value=42
+	// Output: [OK] - Everything is fine - value=42
 	// would exit with code 0
 }
 
 func ExampleExitRaw() {
 	ExitRaw(OK, "Everything is fine")
-	// Output: OK - Everything is fine
+	// Output: [OK] - Everything is fine
 	// would exit with code 0
 }
 
 func ExampleExitError() {
 	err := fmt.Errorf("connection to %s has been timed out", "localhost:12345")
 	ExitError(err)
-	// Output: UNKNOWN - connection to localhost:12345 has been timed out (*errors.errorString)
+	// Output: [UNKNOWN] - connection to localhost:12345 has been timed out (*errors.errorString)
 	// would exit with code 3
 }
 
@@ -35,7 +35,7 @@ func ExampleCatchPanic() {
 	defer CatchPanic()
 
 	panic("something bad happened")
-	// Output: UNKNOWN - Golang encountered a panic: something bad happened
+	// Output: [UNKNOWN] - Golang encountered a panic: something bad happened
 	// would exit with code 3
 }
 
