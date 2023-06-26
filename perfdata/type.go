4e4b05cc167341eb09c5ed5eadcf77969fe66288
Markus Opolka <markus.opolka@netways.de>
2023-06-26 10:31:58 +0200

    Refactor perfdata
     - Moved util.go into type.go since 'util' is too generic a name
     - Made formatting function private, since they should not be used outside the module
     - Refactored tests to be table driven
     - Less restrictive character set for labels

--- perfdata/type.go
+++ perfdata/type.go
@@ -1,9 +1,31 @@
 package perfdata
 
 import (
+	"fmt"
 	"github.com/NETWAYS/go-check"
 	"strings"
 )
+
+// Replace not allowed characters inside a label
+var replacer = strings.NewReplacer("=", "_", "`", "_", "'", "_")
+
+// formatNumeric returns a string representation of various possible numerics
+//
+// This supports most internal types of Go and all fmt.Stringer interfaces.
+func formatNumeric(value interface{}) string {
+	switch v := value.(type) {
+	case float64:
+		return check.FormatFloat(v)
+	case float32:
+		return check.FormatFloat(float64(v))
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return fmt.Sprintf("%d", v)
+	case fmt.Stringer, string:
+		return fmt.Sprint(v)
+	default:
+		panic(fmt.Sprintf("unsupported type for perfdata: %T", value))
+	}
+}
 
 // Perfdata represents all properties of performance data for Icinga
 //
@@ -31,9 +53,14 @@ type Perfdata struct {
 func (p Perfdata) String() string {
 	var sb strings.Builder
 
-	sb.WriteString(FormatLabel(p.Label) + "=")
+	// Add quotes if string contains any whitespace
+	if strings.ContainsAny(p.Label, "\t\n\f\r ") {
+		sb.WriteString(`'` + replacer.Replace(p.Label) + `'` + "=")
+	} else {
+		sb.WriteString(replacer.Replace(p.Label) + "=")
+	}
 
-	sb.WriteString(FormatNumeric(p.Value))
+	sb.WriteString(formatNumeric(p.Value))
 	sb.WriteString(p.Uom)
 
 	// Thresholds
@@ -50,7 +77,7 @@ func (p Perfdata) String() string {
 		sb.WriteString(";")
 
 		if value != nil {
-			sb.WriteString(FormatNumeric(value))
+			sb.WriteString(formatNumeric(value))
 		}
 	}
 
