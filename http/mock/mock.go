f074560889b046b87f81d2b3a4ef365c38c6b7b1
Markus Opolka <markus.opolka@netways.de>
2023-06-26 13:02:05 +0200

    Add deprecation notice to http/mock module

--- http/mock/mock.go
+++ http/mock/mock.go
@@ -3,4 +3,5 @@
 // Features:
 // - RegisterQueryMapResponder form query based based on a QueryMap
 // - ActivateRecorder to record HTTP requests during the development of tests and examples
+// Deprecated: Will be removed in a future version
 package checkhttpmock
