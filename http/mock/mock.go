f59af429fc04f7361413b8a03e75fc40ec432333
Lorenz Kästle <lorenz.kaestle@netways.de>
2023-06-29 11:56:20 +0200

    Remove http sub package to focus the purpose of this library

--- http/mock/mock.go
+++
@@ -1,7 +0,0 @@
-// Helper package to add more functions to httpmock (github.com/jarcoal/httpmock)
-//
-// Features:
-// - RegisterQueryMapResponder form query based based on a QueryMap
-// - ActivateRecorder to record HTTP requests during the development of tests and examples
-// Deprecated: Will be removed in a future version
-package checkhttpmock
