e9ecd19daae4119c58c25c81b56a299c9861a14c
Markus Opolka <markus.opolka@netways.de>
2023-06-29 11:45:32 +0200

    Refactor perfdata strings (#88)
    * Add benchmark for perfdata
    
     - The perfdata code does a lot of string operations, we might want to optimize it in the future
     - Example usage go test -run=. -bench=BenchmarkPerfdataString -count=10 ./... | tee 1.benchmark
    
    * Refactor perfdata
    
     - Moved util.go into type.go since 'util' is too generic a name
     - Made formatting function private, since they should not be used outside the module
     - Refactored tests to be table driven
     - Less restrictive character set for labels

--- perfdata/type_test.go
+++ perfdata/type_test.go
@@ -1,16 +1,17 @@
 package perfdata
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/NETWAYS/go-check"
 	"github.com/stretchr/testify/assert"
 )
 
-func ExamplePerfdata() {
+func BenchmarkPerfdataString(b *testing.B) {
+	b.ReportAllocs()
+
 	perf := Perfdata{
-		Label: "test",
+		Label: "test test=test",
 		Value: 10.1,
 		Uom:   "%",
 		Warn:  &check.Threshold{Upper: 80},
@@ -18,39 +19,64 @@ func ExamplePerfdata() {
 		Min:   0,
 		Max:   100}
 
-	fmt.Println(perf)
-
-	// Output:
-	// test=10.1%;80;90;0;100
+	for i := 0; i < b.N; i++ {
+		p := perf.String()
+		_ = p
+	}
 }
 
-func TestPerfdata(t *testing.T) {
-	perf := Perfdata{
-		Label: "test",
-		Value: 2,
+func TestRenderPerfdata(t *testing.T) {
+	testcases := map[string]struct {
+		perf     Perfdata
+		expected string
+	}{
+		"simple": {
+			perf: Perfdata{
+				Label: "test",
+				Value: 2,
+			},
+			expected: "test=2",
+		},
+		"with-special-chars": {
+			perf: Perfdata{
+				Label: "test_🖥️_'test",
+				Value: 2,
+			},
+			expected: "test_🖥️__test=2",
+		},
+		"with-uom": {
+			perf: Perfdata{
+				Label: "test",
+				Value: 2,
+				Uom:   "%",
+			},
+			expected: "test=2%",
+		},
+		"with-thresholds": {
+			perf: Perfdata{
+				Label: "foo bar",
+				Value: 2.76,
+				Uom:   "m",
+				Warn:  &check.Threshold{Lower: 10, Upper: 25, Inside: true},
+				Crit:  &check.Threshold{Lower: 15, Upper: 20, Inside: false},
+			},
+			expected: "'foo bar'=2.76m;@10:25;15:20",
+		},
 	}
 
-	assert.Equal(t, "test=2", perf.String())
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, tc.perf.String())
+		})
+	}
 }
 
-func TestPerfdata2(t *testing.T) {
-	perf := Perfdata{
-		Label: "test",
-		Value: 2,
-		Uom:   "%",
-	}
-
-	assert.Equal(t, "test=2%", perf.String())
-}
-
-func TestPerfdata3(t *testing.T) {
-	perf := Perfdata{
-		Label: "foo bar",
-		Value: 2.76,
-		Uom:   "m",
-		Warn:  &check.Threshold{Lower: 10, Upper: 25, Inside: true},
-		Crit:  &check.Threshold{Lower: 15, Upper: 20, Inside: false},
-	}
-
-	assert.Equal(t, "'foo bar'=2.76m;@10:25;15:20", perf.String())
+func TestFormatNumeric(t *testing.T) {
+	assert.Equal(t, "10", formatNumeric(10))
+	assert.Equal(t, "-10", formatNumeric(-10))
+	assert.Equal(t, "10", formatNumeric(uint8(10)))
+	assert.Equal(t, "1234.567", formatNumeric(1234.567))
+	assert.Equal(t, "1234.567", formatNumeric(float32(1234.567)))
+	assert.Equal(t, "1234.567", formatNumeric("1234.567"))
+	assert.Equal(t, "1234567890.988", formatNumeric(1234567890.9877))
 }
