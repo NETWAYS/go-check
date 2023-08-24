51eb0ffd9e1c882a0b784bbcd658464bbb26a15e
Markus Opolka <markus.opolka@netways.de>
2023-08-24 16:34:12 +0200

    Add constants for check status strings

--- status_test.go
+++ status_test.go
@@ -1,10 +1,39 @@
 package check
 
 import (
-	"fmt"
+	"testing"
 )
 
-func ExampleStatusText() {
-	fmt.Println(StatusText(OK), StatusText(Warning), StatusText(Critical), StatusText(Unknown))
-	// Output: OK WARNING CRITICAL UNKNOWN
+func TestStatusText(t *testing.T) {
+	testcases := map[string]struct {
+		input    int
+		expected string
+	}{
+		"OK": {
+			input:    0,
+			expected: "OK",
+		},
+		"WARNING": {
+			input:    1,
+			expected: "WARNING",
+		},
+		"CRITICAL": {
+			input:    2,
+			expected: "CRITICAL",
+		},
+		"UNKNOWN": {
+			input:    3,
+			expected: "UNKNOWN",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			actual := StatusText(tc.input)
+
+			if actual != tc.expected {
+				t.Error("\nActual: ", actual, "\nExpected: ", tc.expected)
+			}
+		})
+	}
 }
