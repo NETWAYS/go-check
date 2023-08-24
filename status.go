9d7cf4bf1ca661dc81a03b9fe94c861d911fdf6d
Markus Opolka <markus.opolka@netways.de>
2023-08-24 16:41:29 +0200

    Add constants for check status strings (#100)

--- status.go
+++ status.go
@@ -2,26 +2,30 @@ package check
 
 const (
 	// OK means everything is fine
-	OK = 0
+	OK       = 0
+	OKString = "OK"
 	// Warning means there is a problem the admin should review
-	Warning = 1
+	Warning       = 1
+	WarningString = "WARNING"
 	// Critical means there is a problem that requires immediate action
-	Critical = 2
+	Critical       = 2
+	CriticalString = "CRITICAL"
 	// Unknown means the status can not be determined, probably due to an error or something missing
-	Unknown = 3
+	Unknown       = 3
+	UnknownString = "UNKNOWN"
 )
 
 // StatusText returns the string corresponding to a state
 func StatusText(status int) string {
 	switch status {
 	case OK:
-		return "OK"
+		return OKString
 	case Warning:
-		return "WARNING"
+		return WarningString
 	case Critical:
-		return "CRITICAL"
+		return CriticalString
 	case Unknown:
 	}
 
-	return "UNKNOWN"
+	return UnknownString
 }
